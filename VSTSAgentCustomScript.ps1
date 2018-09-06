ff29e51957ef628807aa8f552d7980a990ce40be
aatif912 <aatif912@gmail.com>
2018-09-06 11:58:47 +0530

    Add files via upload

---
+++ VSTSAgentCustomScript.ps1
@@ -0,0 +1,15 @@
+﻿[Parameter(Mandatory = $true)] [String]$Version,
+[Parameter(Mandatory = $true)] [String]$AccessToken,
+[Parameter(Mandatory = $true)] [String]$AgentPoolName
+
+
+if (!(Test-Path -Path C:\temp)) {
+    New-Item -ItemType directory -Path C:\temp
+}
+
+Invoke-WebRequest -UseBasicParsing -Uri https://vstsagentpackage.azureedge.net/agent/$Version/vsts-agent-win-x64-$Version.zip -OutFile C:\temp\vsts-agent-win-x64-$Version.zip
+Expand-Archive -Path C:\temp\vsts-agent-win-x64-$Version.zip -DestinationPath C:\vsts-agent -Verbose -Force
+rm -Force C:\temp\vsts-agent-win-x64-$Version.zip
+Set-Location -Path C:\vsts-agent
+.\config.cmd --unattended --url https://shcgravity.visualstudio.com --auth pat --token $AccessToken --pool $AgentPoolName --agent $env:COMPUTERNAME --runAsService --work C:\vsts-agent\_work --replace --acceptTeeEula
+
